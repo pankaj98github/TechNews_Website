c99cbe99cd942edcb02d6efaaba93905d74f398c
Pankaj <pankajkumbhare111@gmail.com>
2022-08-10 19:15:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Pagination from "./components/Pagination";
+import Search from "./components/Search";
+import Stories from "./components/Stories";
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Search />
+      <Pagination />
+      <Stories />
+    </>
   );
 }
 
